011361eb54f01b2b261d97d77a2984dc102346ed
Ryusei <sidapple@gmail.com>
2022-06-15 22:12:02 +0900

    update 不要なコメント削除

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import SignUp from './components/SignUp';
 import Login from './components/Login';
 import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
-import PublicRoute from './components/PublicRoute';
-
-import { useAuthContext } from './context/AuthContext';
 
 function App() {
   return (
@@ -17,12 +13,8 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path={`/`} element={<Home />} />
-              {/* <PrivateRoute path={`/`} element={<Home />} /> */}
-
               <Route path={`/signup`} element={<SignUp />} />
               <Route path={`/login`} element={<Login />} />
-              {/* <PublicRoute path="/signup" component={<SignUp />} />
-              <PublicRoute path="/login" component={<Login />} /> */}
             </Routes>
           </BrowserRouter>
         </div>
